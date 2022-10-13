74a40d92e93c342a6d527cb9226c4067e2b94be1
armando salamanca ayon <asalamanca.ayon@gmail.com>
2022-10-13 17:51:39 -0500

    delete the dist files in repository

--- dist/interfaces/user-interface.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var rolUser;
-(function (rolUser) {
-    rolUser[rolUser["Provider"] = 1] = "Provider";
-    rolUser[rolUser["Business"] = 2] = "Business";
-    rolUser[rolUser["Agent"] = 3] = "Agent";
-})(rolUser || (rolUser = {}));
