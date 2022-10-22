30e112b3b7382b9eaea7ab5cdb817a6fe7ee1d01
ojimenez <oomarjimenez@gmail.com>
2022-10-22 13:09:07 -0500

    Se agrega .editorconfig

--- controllers/auth/auth-controller.ts
+++ controllers/auth/auth-controller.ts
@@ -4,17 +4,17 @@ import { responseError, responseSuccess } from "../../helpers/responseManager";
 import { loginUserStore } from "./auth-store";
 
 export const auth = async (req: Request, res: Response) => {
-    try {
-      const response = await loginUserStore(req.body);
-      if (typeof response === "string") {
-        responseError(res, response, boom.serverUnavailable());
-      } else if (typeof response === "object") {
-        response?.token && typeof response?.token === "string"
-          ? responseSuccess(res, response, 201)
-          : responseError(res, response, boom.badRequest());
-      }
-    } catch (error) {
-      console.error("[loginUserError]: ", error);
-      return "Hay un error inesperado, intenta de nuevo mas tarde";
+  try {
+    const response = await loginUserStore(req.body);
+    if (typeof response === "string") {
+      responseError(res, response, boom.serverUnavailable());
+    } else if (typeof response === "object") {
+      response?.token && typeof response?.token === "string"
+        ? responseSuccess(res, response, 201)
+        : responseError(res, response, boom.badRequest());
     }
-  };
+  } catch (error) {
+    console.error("[loginUserError]: ", error);
+    return "Hay un error inesperado, intenta de nuevo mas tarde";
+  }
+};
